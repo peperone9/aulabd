5dfbe96b0c82475abc9c41e3c7a65547b6e7cb24
Pedro <46246172+peperone9@users.noreply.github.com>
2022-06-02 13:59:38 -0300

    Add files via upload

---
+++ FormularioBasico/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace FormularioBasico
+{
+    partial class func
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtFunc = new System.Windows.Forms.TextBox();
+            this.txtCpf = new System.Windows.Forms.MaskedTextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(56, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(71, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CPF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(35, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Funcionario";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(159, 72);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(124, 23);
+            this.txtCodigo.TabIndex = 3;
+            // 
+            // txtFunc
+            // 
+            this.txtFunc.Location = new System.Drawing.Point(159, 126);
+            this.txtFunc.Name = "txtFunc";
+            this.txtFunc.Size = new System.Drawing.Size(225, 23);
+            this.txtFunc.TabIndex = 4;
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(159, 178);
+            this.txtCpf.Mask = "000-000-000-00";
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(93, 23);
+            this.txtCpf.TabIndex = 5;
+            this.txtCpf.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(439, 29);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(90, 44);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Pesquisar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(439, 79);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(90, 45);
+            this.btnRegister.TabIndex = 7;
+            this.btnRegister.Text = "Cadastrar";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(439, 130);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(90, 39);
+            this.btnChange.TabIndex = 8;
+            this.btnChange.Text = "Alterar";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(439, 175);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(90, 47);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Excluir";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // func
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 297);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.txtFunc);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "func";
+            this.Text = "Formulario";
+            this.Load += new System.EventHandler(this.Funcionario_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtFunc;
+        private System.Windows.Forms.MaskedTextBox txtCpf;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ FormularioBasico/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FormularioBasico
+{
+    public partial class func : Form
+    {
+       
+        public func()
+        {
+            InitializeComponent();
+        }
+        //Conecta com o bd no sqlserver
+        SqlConnection cn = new SqlConnection(@"Data Source=LAB1-20;Initial Catalog=db_turmaB;integrated security=false; User Id=sa; password=1234567");
+        //Faz os comandos sql
+        SqlCommand cm = new SqlCommand();
+        //lê o bd
+        SqlDataReader rd; 
+        private void Funcionario_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //btnDelete
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //btnChange
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //btnRegister
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int cd_func;
+            if(!int.TryParse(txtCodigo.Text, out cd_func))
+            {
+                MessageBox.Show("Código inválido !! Digitar um valor inteiro");
+            }
+   
+        }
+
+        //btnSearch
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
